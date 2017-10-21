e13fe66d15f4bfad81b9dd92c68524197c9af71d
giwrgostheod <giwrgosrtheod@gmail.com>
2017-10-21 14:54:34 +0100

    Fixed SaberProjectRule & SaberFilterRule

--- src/main/java/calcite/planner/physical/rules/SaberFilterRule.java
+++ src/main/java/calcite/planner/physical/rules/SaberFilterRule.java
@@ -5,11 +5,10 @@ import java.util.Set;
 
 import org.apache.calcite.rel.RelNode;
 import org.apache.calcite.rel.logical.LogicalFilter;
-import org.apache.calcite.rex.RexNode;
 import org.apache.calcite.util.Pair;
 
-import calcite.planner.physical.PredicateUtil;
 import calcite.planner.physical.SaberRule;
+import calcite.planner.physical.rules.util.SaberFilterUtil;
 import uk.ac.imperial.lsds.saber.ITupleSchema;
 import uk.ac.imperial.lsds.saber.Query;
 import uk.ac.imperial.lsds.saber.QueryConf;
@@ -17,9 +16,7 @@ import uk.ac.imperial.lsds.saber.QueryOperator;
 import uk.ac.imperial.lsds.saber.WindowDefinition;
 import uk.ac.imperial.lsds.saber.WindowDefinition.WindowType;
 import uk.ac.imperial.lsds.saber.cql.operators.IOperatorCode;
-import uk.ac.imperial.lsds.saber.cql.operators.cpu.Selection;
-import uk.ac.imperial.lsds.saber.cql.operators.gpu.SelectionKernel;
-import uk.ac.imperial.lsds.saber.cql.predicates.IPredicate;
+
 
 public class SaberFilterRule implements SaberRule {
 	
@@ -47,36 +44,31 @@ public class SaberFilterRule implements SaberRule {
 	}
 	
 	public void prepareRule() {
-	
+				
 		WindowType windowType = (window!=null) ? window.getWindowType() : WindowType.ROW_BASED;
 		long windowRange = (window!=null) ? window.getSize() : 1;
 		long windowSlide = (window!=null) ? window.getSlide() : 1;
-		LogicalFilter filter = (LogicalFilter) rel;
-		RexNode condition = filter.getCondition();
 		
-		QueryConf queryConf = new QueryConf (batchSize);
-		
-		window = new WindowDefinition (windowType, windowRange, windowSlide);
-		
-		PredicateUtil predicateHelper = new PredicateUtil();
-		Pair<RexNode, IPredicate> pair = predicateHelper.getCondition(condition, 0);
-		IPredicate predicate = pair.right;
-		System.out.println("Filter Expr is : "+ predicate.toString());
-		
-		cpuCode = new Selection (predicate);
-		gpuCode = new SelectionKernel (schema, predicate, null, batchSize);
-		
-		QueryOperator operator;
-		operator = new QueryOperator (cpuCode, gpuCode);
-		
+		QueryConf queryConf = new QueryConf (batchSize);		
+		window = new WindowDefinition (windowType, windowRange, windowSlide);	
 		Set<QueryOperator> operators = new HashSet<QueryOperator>();
-		operators.add(operator);
 		
-		WindowDefinition filterWindow = new WindowDefinition (WindowType.ROW_BASED, 1, 1);
-		System.out.println("Window is : " + filterWindow.getWindowType().toString() + " with " + filterWindow.toString());
+		LogicalFilter logFilter = (LogicalFilter) rel;
 		
-		query = new Query (queryId, operators, schema, filterWindow, null, null, queryConf, timestampReference);		
-		outputSchema = schema;			
+		SaberFilterUtil filter = new SaberFilterUtil(logFilter.getCondition(), batchSize, schema);
+		filter.build();
+		
+		operators.add(filter.getOperator());
+		cpuCode = filter.getCpuCode();
+		gpuCode = filter.getGpuCode();
+		outputSchema = filter.getOutputSchema();
+	    
+		WindowDefinition executionWindow = new WindowDefinition (WindowType.ROW_BASED, 1, 1);
+		System.out.println("Window is : " + executionWindow.getWindowType().toString() + " with " + executionWindow.toString());
+		
+		query = new Query (queryId, operators, schema, executionWindow, null, null, queryConf, timestampReference);
+		//resize the window according to possible changes from input
+		window = new WindowDefinition (windowType, windowRange, windowSlide);
 	}
 	
 	public ITupleSchema getOutputSchema() {
--- src/main/java/calcite/planner/physical/rules/SaberProjectRule.java
+++ src/main/java/calcite/planner/physical/rules/SaberProjectRule.java
@@ -1,30 +1,22 @@
 package calcite.planner.physical.rules;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import org.apache.calcite.rel.RelNode;
-import org.apache.calcite.rex.RexNode;
+import org.apache.calcite.rel.logical.LogicalProject;
 import org.apache.calcite.util.Pair;
 
-import calcite.planner.physical.ExpressionBuilder;
 import calcite.planner.physical.SaberRule;
+import calcite.planner.physical.rules.util.SaberProjectUtil;
 import uk.ac.imperial.lsds.saber.ITupleSchema;
 import uk.ac.imperial.lsds.saber.Query;
 import uk.ac.imperial.lsds.saber.QueryConf;
 import uk.ac.imperial.lsds.saber.QueryOperator;
 import uk.ac.imperial.lsds.saber.WindowDefinition;
-import uk.ac.imperial.lsds.saber.TupleSchema.PrimitiveType;
 import uk.ac.imperial.lsds.saber.WindowDefinition.WindowType;
-import uk.ac.imperial.lsds.saber.cql.expressions.Expression;
-import uk.ac.imperial.lsds.saber.cql.expressions.ExpressionsUtil;
-import uk.ac.imperial.lsds.saber.cql.expressions.floats.FloatColumnReference;
-import uk.ac.imperial.lsds.saber.cql.expressions.ints.IntColumnReference;
-import uk.ac.imperial.lsds.saber.cql.expressions.longs.LongColumnReference;
 import uk.ac.imperial.lsds.saber.cql.operators.IOperatorCode;
-import uk.ac.imperial.lsds.saber.cql.operators.cpu.Projection;
-import uk.ac.imperial.lsds.saber.cql.operators.gpu.ProjectionKernel;
+
 
 /*Wrong result after join*/
 public class SaberProjectRule implements SaberRule {
@@ -59,97 +51,32 @@ public class SaberProjectRule implements SaberRule {
 	
 		WindowType windowType = (window!=null) ? window.getWindowType() : WindowType.ROW_BASED;
 		long windowRange = (window!=null) ? window.getSize() : 1;
-		long windowSlide = (window!=null) ? window.getSlide() : 1; //maybe keep the (1,1) window for project and filter??
-		int projectedAttributes = 0;
-		int expressionDepth = 1;
-		int numberOfInputAttrs = schema.numberOfAttributes();
-				
-		QueryConf queryConf = new QueryConf (batchSize);
+		long windowSlide = (window!=null) ? window.getSlide() : 1;
 		
-		window = new WindowDefinition (windowType, windowRange, windowSlide);
-		
-		List<RexNode> projectedAttrs = rel.getChildExps(); 
-		
-		projectedAttributes = projectedAttrs.size();
-		
-		Expression [] expressions = new Expression [projectedAttributes];
-		/* Always project the timestamp */
-		//expressions[0] = new LongColumnReference(0);
-			
-		int column;
-		int i = 0;
-		for (RexNode attr : projectedAttrs){
-			//System.out.println(attr.toString());
-			if (attr.getKind().toString().equals("INPUT_REF")) {				
-				column = Integer.parseInt(attr.toString().replace("$", ""));
-				if ((windowBarrier >= 0) && (column >= windowOffset) && (column > 0)){ //fix the offset when the previous operator was LogicalWindow
-					//if (column==windowOffset)column+=1; 
-					column -= windowBarrier;	
-				}				
-				if ((windowBarrier < 0) && (column >= windowOffset) && (windowOffset >0)) //fix the offset when the previous operator was LogicalAggregate
-					column -= 1;
-				
-				if (column < 0 && windowBarrier > 0) column+= windowBarrier;
-				if (column >= numberOfInputAttrs) column -= 1;
-				
-				if (schema.getAttributeType(column).equals(PrimitiveType.INT))
-					expressions[i] = new IntColumnReference (column);
-				else if (schema.getAttributeType(column).equals(PrimitiveType.FLOAT)) 
-					expressions[i] = new FloatColumnReference (column);
-				else
-					expressions[i] = new LongColumnReference (column);
-			} else { 
-				//pass the windowOffset to more complex expressions
-				Pair<Expression, Pair<Integer, Integer>> pair = new ExpressionBuilder(attr, windowOffset).build();			
-				expressions[i] = pair.left;
-				if (pair.right.left> 0) {
-					windowRange = pair.right.left;
-					windowSlide = pair.right.right;
-					windowType = WindowType.RANGE_BASED;
-				}
-			}			
-			i++;
-		}
-
-		/*Creating output Schema*/
-		outputSchema = ExpressionsUtil.getTupleSchemaFromExpressions(expressions);
-		i = 0;
-		for (RexNode attr : projectedAttrs){
-			if (attr.getKind().toString().equals("INPUT_REF")) {
-				column = Integer.parseInt(attr.toString().replace("$", ""));
-				if ((windowBarrier >= 0) && (column >= windowOffset) && (column > 0)){ //fix the offset when the previous operator was LogicalWindow
-					//if (column==windowOffset)column+=1; 
-					column -= windowBarrier;	
-				}				
-				if ((windowBarrier < 0) && (column >= windowOffset) && (windowOffset >0)) //fix the offset when the previous operator was LogicalAggregate
-					column -= 1;
-				
-				if (column < 0 && windowBarrier > 0) column+= windowBarrier;
-				if (column >= numberOfInputAttrs) column -= 1;
-				
-				outputSchema.setAttributeName(i, schema.getAttributeName(column));
-			} else {
-				outputSchema.setAttributeName(i, attr.toString());
-			}			
-			i++;
-		}
-		
-		IOperatorCode cpuCode = new Projection (expressions);
-		IOperatorCode gpuCode = new ProjectionKernel (schema, expressions, batchSize, expressionDepth);
-		
-		QueryOperator operator;
-		operator = new QueryOperator (cpuCode, gpuCode);
-		
+		QueryConf queryConf = new QueryConf (batchSize);		
+		window = new WindowDefinition (windowType, windowRange, windowSlide);	
 		Set<QueryOperator> operators = new HashSet<QueryOperator>();
-		operators.add(operator);
-			
-		WindowDefinition projectionWindow = new WindowDefinition (WindowType.ROW_BASED, 1, 1);
-		System.out.println("Window is : " + projectionWindow.getWindowType().toString() + " with " + projectionWindow.toString());
 		
-		query = new Query (queryId, operators, schema, projectionWindow, null, null, queryConf, timestampReference);
+		LogicalProject logProj = (LogicalProject) rel;
+		
+		SaberProjectUtil proj = new SaberProjectUtil(logProj.getChildExps(), batchSize, schema, window, windowOffset, windowBarrier);
+		proj.build();
+		
+		operators.add(proj.getOperator());
+		
+		windowType = proj.getWindow().getWindowType();
+		windowRange = proj.getWindow().getSize();
+		windowSlide = proj.getWindow().getSlide();
+		outputSchema = proj.getOutputSchema();
+	    
+		WindowDefinition executionWindow = new WindowDefinition (WindowType.ROW_BASED, 1, 1);
+		System.out.println("Window is : " + executionWindow.getWindowType().toString() + " with " + executionWindow.toString());
+		
+		query = new Query (queryId, operators, schema, executionWindow, null, null, queryConf, timestampReference);
+		
 		//resize the window according to possible changes from input
 		window = new WindowDefinition (windowType, windowRange, windowSlide);
-		//System.out.println(window.toString());
+		System.out.println("The window passed in the next operator is:" + window.toString());
 	}
 		
 	public ITupleSchema getOutputSchema(){
